19c9514e95399ba4e6331557d2726756e7f78737
rucha293-dev <charual.rr.rr@gmail.com>
2026-05-28 16:29:46 +0530

    Update behavior_planner_FSM (2).cpp

--- Planning/behavior_planner_FSM.cpp/behavior_planner_FSM (2).cpp
+++ Planning/behavior_planner_FSM.cpp/behavior_planner_FSM (2).cpp
@@ -27,37 +27,21 @@ State BehaviorPlannerFSM::get_closest_waypoint_goal(
   }
 
   // Waypoints at a lookahead distance
-  // NOTE: "GetNext(d)" creates a list of waypoints at an approximate distance
-  // "d" in the direction of the lane. The list contains one waypoint for each
-  // deviation possible.
-
-  // NOTE 2: GetNextUntilLaneEnd(d) returns a list of waypoints a distance "d"
-  // apart. The list goes from the current waypoint to the end of its
-  // lane.
-
   auto lookahead_waypoints = waypoint_0->GetNext(lookahead_distance);
   auto n_wp = lookahead_waypoints.size();
   if (n_wp == 0) {
-    // LOG(INFO) << "Goal wp is a nullptr";
     State waypoint;
     return waypoint;
   }
-  // LOG(INFO) << "BP - Num of Lookahead waypoints: " << n_wp;
 
   waypoint_0 = lookahead_waypoints[lookahead_waypoints.size() - 1];
 
   is_goal_junction = waypoint_0->IsJunction();
-  // LOG(INFO) << "BP - Is Last wp in junction? (0/1): " << is_goal_junction;
   auto cur_junction_id = waypoint_0->GetJunctionId();
   if (is_goal_junction) {
     if (cur_junction_id == _prev_junction_id) {
-      // LOG(INFO) << "BP - Last wp is in same junction as ego. Junction ID: "
-      //          << _prev_junction_id;
       is_goal_junction = false;
     } else {
-      // LOG(INFO) << "BP - Last wp is in different junction than ego. Junction
-      // ID: "
-      //          << cur_junction_id;
       _prev_junction_id = cur_junction_id;
     }
   }
@@ -74,22 +58,13 @@ double BehaviorPlannerFSM::get_look_ahead_distance(const State& ego_state) {
   auto velocity_mag = utils::magnitude(ego_state.velocity);
   auto accel_mag = utils::magnitude(ego_state.acceleration);
 
-  // TODO-Lookahead: One way to find a reasonable lookahead distance is to find
-  // the distance you will need to come to a stop while traveling at speed V and
-  // using a comfortable deceleration.
-  // Use kinematics: d = v^2 / (2 * a_max) to find stopping distance
-  auto look_ahead_distance = (velocity_mag * velocity_mag) / (2.0 * P_MAX_ACCEL);
-  if (look_ahead_distance < 1.0) {
-    look_ahead_distance = velocity_mag * P_LOOKAHEAD_TIME;
-  }
-
-  // LOG(INFO) << "Calculated look_ahead_distance: " << look_ahead_distance;
+  // Lookahead distance = distance to stop from current speed using comfortable
+  // deceleration: d = v^2 / (2 * a_max)
+  auto look_ahead_distance = (velocity_mag * velocity_mag) / (2.0 * P_DECEL_MAX);
 
   look_ahead_distance =
       std::min(std::max(look_ahead_distance, _lookahead_distance_min),
                _lookahead_distance_max);
-
-  // LOG(INFO) << "Final look_ahead_distance: " << look_ahead_distance;
 
   return look_ahead_distance;
 }
@@ -97,7 +72,6 @@ double BehaviorPlannerFSM::get_look_ahead_distance(const State& ego_state) {
 State BehaviorPlannerFSM::get_goal(const State& ego_state,
                                    SharedPtr<cc::Map> map) {
   // Get look-ahead distance based on Ego speed
-
   auto look_ahead_distance = get_look_ahead_distance(ego_state);
 
   // Nearest waypoint on the center of a Driving Lane.
@@ -105,7 +79,6 @@ State BehaviorPlannerFSM::get_goal(const State& ego_state,
   auto goal_wp = get_closest_waypoint_goal(ego_state, map, look_ahead_distance,
                                            is_goal_in_junction);
 
-  // LOG(INFO) << "Is the FINAL goal on a junction: " << is_goal_in_junction;
   string tl_state = "none";
   State goal =
       state_transition(ego_state, goal_wp, is_goal_in_junction, tl_state);
@@ -116,82 +89,56 @@ State BehaviorPlannerFSM::get_goal(const State& ego_state,
 State BehaviorPlannerFSM::state_transition(const State& ego_state, State goal,
                                            bool& is_goal_in_junction,
                                            string tl_state) {
-  // Check with the Behavior Planner to see what we are going to do and
-  // where our next goal is
-  //
-
   goal.acceleration.x = 0;
   goal.acceleration.y = 0;
   goal.acceleration.z = 0;
 
   if (_active_maneuver == FOLLOW_LANE) {
-    // LOG(INFO) << "BP- IN FOLLOW_LANE STATE";
     if (is_goal_in_junction) {
-      // LOG(INFO) << "BP - goal in junction";
-
       _active_maneuver = DECEL_TO_STOP;
-      // LOG(INFO) << "BP - changing to DECEL_TO_STOP";
 
-      // Let's backup a "buffer" distance behind the "STOP" point
-      // LOG(INFO) << "BP- original STOP goal at: " << goal.location.x << ", "
-      //          << goal.location.y;
-
-      // TODO-goal behind the stopping point: put the goal behind the stopping
-      // point (i.e the actual goal location) by "_stop_line_buffer". HINTS:
-      // remember that we need to go back in the opposite direction of the
-      // goal/road, i.e you should use: ang = goal.rotation.yaw + M_PI and then
-      // use cosine and sine to get x and y
-      //
+      // Put goal behind the stopping point by _stop_line_buffer,
+      // in the opposite direction of the road heading
       auto ang = goal.rotation.yaw + M_PI;
       goal.location.x += _stop_line_buffer * std::cos(ang);
       goal.location.y += _stop_line_buffer * std::sin(ang);
 
-      // LOG(INFO) << "BP- new STOP goal at: " << goal.location.x << ", "
-      //          << goal.location.y;
-
-      // TODO-goal speed at stopping point: What should be the goal speed??
+      // At the stopping point we want speed = 0
       goal.velocity.x = 0.0;
       goal.velocity.y = 0.0;
       goal.velocity.z = 0.0;
 
     } else {
-      // TODO-goal speed in nominal state: What should be the goal speed now
-      // that we know we are in nominal state and we can continue freely?
-      // Remember that the speed is a vector
-      // HINT: _speed_limit * std::sin/cos (goal.rotation.yaw);
+      // Nominal state: drive at the speed limit in the direction of travel
       goal.velocity.x = _speed_limit * std::cos(goal.rotation.yaw);
       goal.velocity.y = _speed_limit * std::sin(goal.rotation.yaw);
       goal.velocity.z = 0;
     }
 
   } else if (_active_maneuver == DECEL_TO_STOP) {
-    // LOG(INFO) << "BP- IN DECEL_TO_STOP STATE";
-    goal = _goal;  // maintain the same stopping goal
+    // Maintain the same goal (stop line) while decelerating
+    goal = _goal;
 
     auto distance_to_stop_sign =
         utils::magnitude(goal.location - ego_state.location);
-    // LOG(INFO) << "Ego distance to stop line: " << distance_to_stop_sign;
 
+    // Use distance to stop sign rather than speed (handles teleportation edge case)
     if (distance_to_stop_sign <= P_STOP_THRESHOLD_DISTANCE) {
       _active_maneuver = STOPPED;
       _start_stop_time = std::chrono::high_resolution_clock::now();
-      // LOG(INFO) << "BP - changing to STOPPED";
     }
   } else if (_active_maneuver == STOPPED) {
-    // LOG(INFO) << "BP- IN STOPPED STATE";
-    goal = _goal;  // Keep previous goal. Stay where you are.
+    // Keep the same stop-line goal while stopped
+    goal = _goal;
 
     long long stopped_secs =
         std::chrono::duration_cast<std::chrono::seconds>(
             std::chrono::high_resolution_clock::now() - _start_stop_time)
             .count();
-    // LOG(INFO) << "BP- Stopped for " << stopped_secs << " secs";
 
     if (stopped_secs >= _req_stop_time && tl_state.compare("Red") != 0) {
-      // TODO-move to FOLLOW_LANE state: What state do we want to move to, when
-      // we are "done" at the STOPPED state?
+      // Done waiting; resume lane following
       _active_maneuver = FOLLOW_LANE;
-      // LOG(INFO) << "BP - changing to FOLLOW_LANE";
     }
   }
   _goal = goal;
